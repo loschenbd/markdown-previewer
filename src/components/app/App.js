ffdde4bd804e6c610582cf7ab58f2d3acc7bf496
Benjamin Loschen <loschenbd@gmail.com>
2020-08-21 09:21:39 -0500

    removed previous elements

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,15 +1,12 @@
 import React from 'react';
 import './App.css';
 
-import Box from '@material-ui/core/Box';
-import { Typography } from '@material-ui/core'
+
 import { makeStyles } from '@material-ui/core'
 import BeepButton from '../button';
 
 const useStyles = makeStyles({
   helloThereStyle: {
-    fontStyle: 'oblique',
-    color: 'red'
   }
 });
 
@@ -18,16 +15,7 @@ function App() {
 
   return (
     <div className="App">
-      <Typography className={classes.helloThereStyle} variant="h1" color="primary">
-        Hello There
-      </Typography>
 
-
-      <Box>
-        <BeepButton />
-        <BeepButton />
-        <BeepButton />
-      </Box>
     </div>
   );
 }
