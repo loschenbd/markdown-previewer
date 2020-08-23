acdd09a70713a579c6e35ea885de6e7a5eba6fb0
Benjamin Loschen <loschenbd@gmail.com>
2020-08-23 07:30:01 -0500

    Added marked, markdown input, and markdown printing.

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,14 +1,23 @@
 import React from 'react';
 import './App.css';
-import Markdown from '../markdown'
+import Markdown from '../markdown';
 import Header from "../header";
 
 
 
 import { Grid, Button } from '@material-ui/core'
 
-function App() {
+class App extends React.Component {
+  constructor(props){
 
+    super(props)
+    this.state = {
+      markdown: "",
+    };
+  }
+
+
+render() {
   return (
     <div className="App">
       <Header/>
@@ -16,16 +25,19 @@ function App() {
       <Grid container>
         <Grid item container>
           <Grid xs={0} sm={1}/>
-            <Grid xs={12} sm={10}>
-              <Markdown />
-            </Grid>
+          <Grid xs={12} sm={10}>
+            <Markdown/>
+          </Grid>
           <Grid xs={0} sm={1}/>
         </Grid>
       </Grid>
-      <Button variant="outlined" color="primary" target="_blank" href="https://github.com/loschenbd/markdown-previewer">View Codebase here</Button>
+      <Button variant="outlined" color="primary" target="_blank" href="https://github.com/loschenbd/markdown-previewer">View
+        Codebase here</Button>
     </div>
 
+
   );
+}
 }
 
 export default App;
--- src/components/markdown.js
+++ src/components/markdown.js
@@ -1,34 +1,53 @@
 import React from 'react';
 import {Grid, Paper, TextField, Typography, sizing} from '@material-ui/core'
+let marked = require("marked");
 
-const Markdown = () => {
-  return (
-    <div>
-      <Grid container spacing={4}>
+
+class Markdown extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      markdown: "",
+    };
+  };
+  updateInput(markdown) {
+    this.setState({ markdown })
+  }
+  render() {
+    return (
+
+      <div>
+        <Grid container spacing={4}>
           <Grid item sm={6} xs={12}>
             <Typography>HTML Output</Typography>
             <Paper variant="outlined">
               <TextField
-                style= {{width: "100%", minHeight: 600}}
+                style={{width: "100%", minHeight: 600}}
                 id="editor"
                 label="Markdown Input"
                 multiline
                 rows={30}
                 defaultValue="Enter Markdown"
+                value={this.state.markdown}
                 variant="outlined"
+                onChange={(e) => {
+                  this.updateInput(e.target.value);
+                }}
               />
+              {console.log(this.state.markdown)}
             </Paper>
           </Grid>
 
-        <Grid item sm={6} xs={12}>
-          <Typography>HTML Output</Typography>
-          <Paper id="preview" variant="outlined"  style={{width: "100%", minHeight: 600}}>
-
-          </Paper>
+          <Grid item sm={6} xs={12}>
+            <Typography>HTML Output</Typography>
+            <Paper id="preview" variant="outlined" style={{width: "100%", minHeight: 600}}>
+              <div dangerouslySetInnerHTML={{ __html: marked(this.state.markdown) }}/>
+            </Paper>
+          </Grid>
         </Grid>
-      </Grid>
-    </div>
-  );
-};
+      </div>
+    );
+  };
+}
 
 export default Markdown
