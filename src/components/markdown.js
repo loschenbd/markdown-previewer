aa6e127be3dd5f524ea37cb1ab75cb1e1eb72573
Benjamin Loschen <loschenbd@gmail.com>
2020-08-23 09:23:46 -0500

    Added preset text.

--- src/components/markdown.js
+++ src/components/markdown.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Grid, Paper, TextField, Typography, sizing} from '@material-ui/core'
+import {Grid, Paper, TextField, Typography } from '@material-ui/core'
 let marked = require("marked");
 
 
@@ -7,7 +7,22 @@ class Markdown extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
-      markdown: "",
+      markdown: "# Heading 1\n" +
+      "## Heading 2\n" +
+      "\n" +
+      "[View Codebase](https://loschenbd.github.io/markdown-previewer/)\n" +
+      "\n" +
+      "`let string = \"a block of code`\n" +
+      "\n" +
+      "- First Item\n" +
+      "- Second Item\n" +
+      "- Third item\n" +
+      "\n" +
+      "> “When you no longer resist a powerful emotion like fear,” he continued, “you're free to channel that energy in a more constructive direction. When you hire your problems as bodyguards, they show you how powerful your mind is. Their very fierceness makes you aware of how strong you are.”\n" +
+      "\n" +
+      "![flux logo](https://uploads-ssl.webflow.com/5d5421282086e70dcd3289a4/5d94e2d7d4118f80b8c718a9_Group%2073x.png)\n" +
+      "\n" +
+      "*Some bold text*"
     };
   };
   updateInput(markdown) {
@@ -19,12 +34,11 @@ class Markdown extends React.Component {
       <div>
         <Grid container spacing={4}>
           <Grid item sm={6} xs={12}>
-            <Typography>HTML Output</Typography>
+            <Typography>Markdown Input</Typography>
             <Paper variant="outlined">
               <TextField
                 style={{width: "100%", minHeight: 600}}
                 id="editor"
-                label="Markdown Input"
                 multiline
                 rows={30}
                 defaultValue="Enter Markdown"
@@ -40,8 +54,8 @@ class Markdown extends React.Component {
 
           <Grid item sm={6} xs={12}>
             <Typography>HTML Output</Typography>
-            <Paper id="preview" variant="outlined" style={{width: "100%", minHeight: 600}}>
-              <div dangerouslySetInnerHTML={{ __html: marked(this.state.markdown) }}/>
+            <Paper variant="outlined" style={{width: "100%", minHeight: 600, overflow: "hidden"}}>
+              <div  id="preview" style={{margin: 10}} dangerouslySetInnerHTML={{ __html: marked(this.state.markdown) }}/>
             </Paper>
           </Grid>
         </Grid>
